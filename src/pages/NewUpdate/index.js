cb52c8089db4ed903017aad2b472d358542112b4
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-04 09:17:47 -0300

    27° commit - logica saveOrUpdate implementada

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -1,8 +1,57 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import React, {useState, useEffect} from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function NewUpdate(){
+
+  const[name, setName] = useState('');
+  const[description, setDescription] = useState('');
+  const navigate = useNavigate();
+  // o identificador hero_id é o mesmo definido na rota
+  const{hero_id} = useParams();
+
+  // Create o Update, cria ou atualiza um hero na api
+  async function saveOrUpdate(e){
+    e.preventDefault();
+    const data = {name, description};
+
+    if (hero_id === '0') {
+      try {
+        await api.post('api/v1/heroes', data, {});
+        navigate('/');            
+      } catch (error) {
+        alert('Erro ao salvar!')          
+      }        
+    } else {
+      try {
+        await api.patch(`api/v1/heroes/${hero_id}`, data, {});
+        navigate('/');
+      } catch (error) {
+        alert('Erro ao atualizar!')        
+      }          
+    }
+  }
+  
+  // Carrega um registro especifico na api e seta dados para update
+  async function loadHero(){
+    try {
+      const response = await api.get(`api/v1/heroes/${hero_id}`,{});
+      setName(response.data.name);
+      setDescription(response.data.description);
+    } catch (error) {
+      alert("Erro ao bscar dados na api!");
+      navigate('/');      
+    }
+  }
+
+  // chama loadHero para preencher dados no form
+  useEffect(() => {
+    if (hero_id === '0') {
+      return;      
+    } else {
+      loadHero();      
+    }
+  },[hero_id]);
 
   return(
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -14,20 +63,22 @@ export default function NewUpdate(){
       <Link data-testid="mylink" className="btn btn-dark" 
       style={{marginBottom: '5px'}} to="/">Home</Link>
 
-      <form data-testid="myform">
+      <form data-testid="myform" onSubmit={saveOrUpdate}>
 
          <div className="form-group">
-          <label htmlFor="###">###</label>
-          <input data-testid="input1" id="###" type="text" 
+          <label htmlFor="name">Nome</label>
+          <input data-testid="input1" id="name" type="text" 
           style={{marginBottom: '20px'}} className="form-control" 
-          placeholder="###"></input>
+          placeholder="Nome" value={name}
+          onChange={e => setName(e.target.value)}></input>
         </div>
 
         <div className="form-group">
-          <label htmlFor="###">###</label>
-          <input data-testid="input2" id="###" type="text" 
+          <label htmlFor="description">Descrição</label>
+          <input data-testid="input2" id="description" type="text" 
           style={{marginBottom: '20px'}} className="form-control" 
-          placeholder="###"></input>
+          placeholder="Descrição" value={description}
+          onChange={e => setDescription(e.target.value)}></input>
         </div>
 
         <button data-testid="btnenviar" type="submit" 
